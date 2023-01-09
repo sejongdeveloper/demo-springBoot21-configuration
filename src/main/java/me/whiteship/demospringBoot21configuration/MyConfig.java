f0c4acf0c33b06b209b407e53f4fde65e31add63
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-09 23:23:17 +0900

    의존성 및 프로퍼티 변경 2부

--- src/main/java/me/whiteship/demospringBoot21configuration/MyConfig.java
+++ src/main/java/me/whiteship/demospringBoot21configuration/MyConfig.java
@@ -3,7 +3,7 @@ package me.whiteship.demospringBoot21configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class MyConfig {
     @Bean
     public Object myBean() {
